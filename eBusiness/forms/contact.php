fecaa2cea2c3f4ad63304cefd62aba35ab9e4756
lunazulMac <stephan.hage@gmail.com>
2023-09-18 21:17:56 +0200

    initial commit

--- eBusiness/forms/contact.php
+++ eBusiness/forms/contact.php
@@ -19,9 +19,9 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = "Subscriber";
+  $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject ="Notify me request";
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,7 +33,9 @@
   );
   */
 
+  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
