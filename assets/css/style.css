c357c3ecb07bfb86b63bb50bcb65b8616d6f543d
Stephan remote <stephan@lunazul.dance>
2024-02-01 21:51:42 +0100

    preliminary

--- assets/css/style.css
+++ assets/css/style.css
@@ -1,6 +1,6 @@
 /**
 * Template Name: eBusiness
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
+* Updated: Jan 29 2024 with Bootstrap v5.3.2
 * Template URL: https://bootstrapmade.com/ebusiness-bootstrap-corporate-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
